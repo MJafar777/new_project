99b46bb472b17f038fae56618d0226be96bf2bdb
MJafar777 <94430558+MJafar777@users.noreply.github.com>
2022-04-10 04:34:33 +0500

    Add files via upload

---
+++ buy.js
@@ -0,0 +1,42 @@
+// export const buyProduct = function () {
+//   console.log('maxsulot sotib oldingiz!!');
+// };
+// let a = 5;
+// let b = 6;
+// let c = 7;
+// export { a, b, c };
+// let card = [];
+// export default function (maxsulot, soni) {
+//   card.push({ maxsulot, soni });
+//   console.log(`siz ${maxsulot}dan ${soni} ta oldingiz`);
+// }
+// export let abc = 'hello ';
+
+// let myfunc = async function () {
+//   const country = await fetch('https://restcountries.com/v2/all');
+//   const country1 = await fetch('https://restcountries.com/v2/all');
+//   const country12 = await fetch('https://restcountries.com/v2/all');
+//   console.log(country, country1, country12);
+// };
+// let job = await fetch('https://restcountries.com/v2/all');
+// console.log(job);
+// myfunc();
+// console.log('hi');
+// console.log(country);
+
+// clouser function birmartalik funksiya
+// const online = function () {
+//   const card = [];
+//   const maxsulotlarSoni = 100;
+//   const ishchilarSoni = 10;
+//   const maxsulotSoni = 10_000_000;
+
+//   const sotibOlish = function (nomi, soni) {
+//     card.push({ nomi, soni });
+//     console.log(` ${nomi}, ${soni}  oldingiz`);
+//   };
+//   return { sotibOlish, card, maxsulotlarSoni, ishchilarSoni };
+// };
+// let online1 = online();
+// online1.sotibOlish('apple', 10);
+// console.log(online1.card);
---
+++ script.js
@@ -0,0 +1,24 @@
+// import * as func from './buy.js';
+// console.log(func);
+// // buyProduct();
+// import sotibOl from './buy.js';
+// sotibOl('bmw x6 ', 23);
+// import { abc } from './buy.js';
+// console.log(abc);
+// let a = 5;
+// console.log(a);
+import 'core-js/stable';
+// import * as map from ('./node_modules/leaflet/dist/leaflet-src.js');
+// const mapcha = reqire(leaflet);
+
+// var map = L.map('map').setView([51.505, -0.09], 13);
+
+// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//   attribution:
+//     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+// }).addTo(map);
+
+// L.marker([51.5, -0.09])
+//   .addTo(map)
+//   .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+//   .openPopup();
